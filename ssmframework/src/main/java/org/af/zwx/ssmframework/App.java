70ebc13bbf3d19195074057ff94ba7792ad311bd
a8080240i <504546445@qq.com>
2019-11-26 12:40:01 +0800

    添加配置文件
    Signed-off-by: a8080240i <504546445@qq.com>

---
+++ ssmframework/src/main/java/org/af/zwx/ssmframework/App.java
@@ -0,0 +1,13 @@
+package org.af.zwx.ssmframework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
